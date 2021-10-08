d605774f12d5dcb711d22b6e126701b81f159aa9
EPPA <ramya.eppa@capgemini.com>
2021-10-08 19:59:28 +0530

    modified some code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("welcome");
-            console.write("Azure.net");
+            Console.Write("Azure.net");
         }
     }
 }
