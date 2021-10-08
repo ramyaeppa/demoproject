9129091fc762b0e75089f7ffcbf120c2bd755c27
ramyaeppa <92098276+ramyaeppa@users.noreply.github.com>
2021-10-08 19:52:31 +0530

    added code to program class

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("welcome");
+            console.write("Azure.net");
         }
     }
 }
